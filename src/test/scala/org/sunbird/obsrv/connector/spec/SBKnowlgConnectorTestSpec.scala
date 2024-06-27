d246b549512fcf6e69d2a30301566761371a06b7
Santhosh Vasabhaktula <santhosh@sanketika.in>
2024-06-27 19:15:27 +0530

    feat: v2 Refactoring. Move the entry_topic as a separate field. This is to enable creation of multiple pipelines in the future

--- src/test/scala/org/sunbird/obsrv/connector/spec/SBKnowlgConnectorTestSpec.scala
+++ src/test/scala/org/sunbird/obsrv/connector/spec/SBKnowlgConnectorTestSpec.scala
@@ -41,10 +41,13 @@ class SBKnowlgConnectorTestSpec extends BaseFlinkConnectorSpec with Serializable
   override def getConnectorSource(): IConnectorSource = new SBKnowlgConnectorSource()
 
   override def testFailedEvents(events: util.List[String]): Unit = {
-    events.asScala.head should be ("""{"data":[{"nodeUniqueId":"lex_auth_0133471820488704007",""")
+    events.asScala.size should be (1)
+    Console.println("events.asScala.head", events.asScala.head)
+    events.asScala.head should be ("""{"error":{"error_code":"JSON_FORMAT_ERR","error_msg":"Not a valid json"},"event":"{\"data\":[{\"nodeUniqueId\":\"lex_auth_0133471820488704007\",","connector_ctx":{"connector_id":"sb-knowlg-connector","dataset_id":"d1","connector_instance_id":"c1","connector_type":"source","data_format":"json","entryTopic":"ingest","state":{},"stats":{}}}""")
   }
 
   override def testSuccessEvents(events: util.List[String]): Unit = {
+    events.asScala.size should be (2)
     events.asScala.head should be ("""{"sourceName":"Global Learning & Growth","identifier":"lex_auth_0133471820488704007","sourceShortName":"Global Learning & Growth"}""")
     events.asScala.last should be ("""{"sourceName":"Global Learning & Growth","identifier":"lex_auth_013326639360794624170","sourceShortName":"Global Learning & Growth"}""")
   }
